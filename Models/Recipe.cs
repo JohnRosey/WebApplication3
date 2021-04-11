700ac346086af17806cc0c84d019f5d4b2f7072e
Ismael <proismaelgans2017@gmail.com>
2021-04-10 21:02:33 -0400

    Ajoutez des fichiers projet.

---
+++ Models/Recipe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Recipe
+    {
+        public int id {
+            get;
+            set;
+        }
+        [Display(Name="Nom du plat")]
+        public string foodName { get; set; }
+        public string Description { get; set; }
+         
+        public string Image { get; set; }
+
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public List<Recipe>Recipes {
+            get;
+            set;
+        }
+        
+        public void OnGet()
+        {
+             String myStr = "Allo UQTR";
+            Recipes = new List<Recipe>()
+            {
+                new Recipe {id = 1, foodName = "Poutine", Description = "Un repas du Quebec", Image = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/La_Banquise_Poutine.jpg/280px-La_Banquise_Poutine.jpg"},
+                new Recipe {id = 2, foodName = "Pizza", Description = "Un repas de l' Amerique", Image = "https://www.hervecuisine.com/wp-content/uploads/2020/03/recette-tarte-pizza-730x520.jpg.webp"},
+            };
+
+        }
+    }
+}
